270cd44722516d8c7e7b366a81cf520b12d01ee9
Douglas <douglasonkeo3@gmail.com>
2022-07-04 14:45:32 +0300

    Adding a login Query and login in the schema

--- GraphQL_Backend/graphql/schema.js
+++ GraphQL_Backend/graphql/schema.js
@@ -19,6 +19,10 @@ module.exports = buildSchema(`
         status: String!
         posts: [Post!]!
     }
+    type authData {
+        token: String!
+        userId: String!
+    }
 
     input UserInputData {
         email: String!
@@ -27,7 +31,7 @@ module.exports = buildSchema(`
     }
 
     type RootQuery {
-        hello: String
+        login(email: String!, password: String!): authData! 
     }
 
     type RootMutation {
